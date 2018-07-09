1585ed2e7cee827c7310bb0d34c6f44c46434f77
NNiehof <nynkeniehof@gmail.com>
2018-07-09 14:58:49 +0200

    cleaned up psychometricCurveFit

--- Analysis/psychometricCurveFit.py
+++ Analysis/psychometricCurveFit.py
@@ -6,7 +6,6 @@ import numpy as np
 from itertools import chain
 from scipy.special import erfc
 from scipy.optimize import curve_fit
-import matplotlib.pyplot as plt
 
 
 def success_ratio(x,y):
@@ -17,17 +16,18 @@ def success_ratio(x,y):
     x = np.array(x)
     y = np.array(y)
     xdata = []
-    sRatio = []
+    s_ratio = []
     freqs = []
-    for xval in set(x): # for each x-value, find all responses
+    for xval in set(x):
+        # for each x-value, find all responses
         ind = (x == xval)
         yresp = y[ind]
         yfreq = len(yresp)
         ratio = sum(yresp)/yfreq
         xdata.append(xval)
-        sRatio.append(ratio)
+        s_ratio.append(ratio)
         freqs.append(yfreq)
-    data = np.vstack((xdata,sRatio,freqs))
+    data = np.vstack((xdata, s_ratio, freqs))
     return data
 
 
@@ -46,12 +46,11 @@ def psyfun(x,alpha,beta,gamma,llambda):
     Cumulative Gaussian with the length of x.
     """
     x = np.array(x)
-    psy = gamma + (1-gamma-llambda)* 0.5* erfc(-beta * (x-alpha)/np.sqrt(2))
-    psy = gamma + (1-2*gamma)* 0.5* erfc(-beta * (x-alpha)/np.sqrt(2))
+    psy = gamma + (1-gamma-llambda) * 0.5 * erfc(-beta * (x-alpha)/np.sqrt(2))
     return psy
 
 
-def psyfunGamEqLamb(x,alpha,beta,gamma):
+def psyfun_gam_eq_lamb(x,alpha,beta,gamma):
     """ Cumulative Gaussian function where gamma is assumed equal to lambda.
 
     Arguments
@@ -70,7 +69,7 @@ def psyfunGamEqLamb(x,alpha,beta,gamma):
     return psy
 
     
-def psyfunAlphaBeta(x,alpha,beta):
+def psyfun_alpha_beta(x,alpha,beta):
     """ Cumulative Gaussian function with mean alpha and slope beta.
     Lapse rate and guess rate are assumed zero, and not fitted. This can be used when
     data sets are such that lapse and guess rates are not informative or cannot be fitted.
@@ -103,15 +102,18 @@ def fit_sigmoid(data, xdata_range=None, gamma_eq_lambda=False,
         ydata.append(ylist*int(freqs[ind]))
     xdata = list(chain.from_iterable(xdata))
     ydata = list(chain.from_iterable(ydata))
+    params = []
+    psycurve = []
     
     if xdata_range is None:
         xdata_range = xdata
     if no_lapses:
-        params, covar = curve_fit(psyfunAlphaBeta, xdata, ydata, bounds=([-np.inf, 0], np.inf), max_nfev=5000) # bounds set such that slope must be positive (lower bound=0)
-        psycurve = psyfunAlphaBeta(xdata_range, params[0], params[1])
+        # boundaries set such that slope must be positive (lower bound=0)
+        params, covar = curve_fit(psyfun_alpha_beta, xdata, ydata, bounds=([-np.inf, 0], np.inf), max_nfev=5000)
+        psycurve = psyfun_alpha_beta(xdata_range, params[0], params[1])
     elif gamma_eq_lambda:
-        params, covar = curve_fit(psyfunGamEqLamb, xdata, ydata, absolute_sigma=True, bounds=bounds)
-        psycurve = psyfunGamEqLamb(xdata_range, *params)
+        params, covar = curve_fit(psyfun_gam_eq_lamb, xdata, ydata, absolute_sigma=True, bounds=bounds)
+        psycurve = psyfun_gam_eq_lamb(xdata_range, *params)
     elif not gamma_eq_lambda:
         params, covar = curve_fit(psyfun, xdata, ydata, absolute_sigma=True, bounds=bounds)
         psycurve = psyfun(xdata_range, *params)
