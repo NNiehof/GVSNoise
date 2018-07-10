1ea658ff823e813616cd8cee52a66555e33ba2ff
NNiehof <nynkeniehof@gmail.com>
2018-07-10 16:28:46 +0200

    rerunning missed trials - work in progress

--- Experiment/blockedStimulus.py
+++ Experiment/blockedStimulus.py
@@ -14,32 +14,45 @@ class BlockStim:
         :param frame_angles: tilts in degrees
         :param currents: maximum current in mA
         """
-        trials = []
+        self.block_size = block_size
+        self.trial_list = []
+        self.trials = []
+        self.gvs_missed = []
+        self.no_gvs_missed = []
         for curr in currents:
             for stim, repeat in zip(stimulus_range, repeats):
                 for rep in range(repeat):
                     if len(frame_angles) > 1:
                         for frame in frame_angles:
-                            trials.append([stim, frame, curr])
+                            self.trials.append([stim, frame, curr])
                     else:
-                        trials.append([stim, frame_angles, curr])
+                        # this is to avoid iterating over letters when the
+                        # only condition is "noframe" (string)
+                        self.trials.append([stim, frame_angles, curr])
         # split trial list in half over GVS current condition
-        half = int(len(trials) / 2)
-        first_half = trials[0:half]
+        # TODO: make this flexible for > 2 current conditions
+        half = int(len(self.trials) / 2)
+        first_half = self.trials[0:half]
         shuffle(first_half)
-        second_half = trials[half::]
+        second_half = self.trials[half::]
         shuffle(second_half)
+        self.alternate_blocks(first_half, second_half)
 
-        # alternate current conditions
-        n_blocks = int(len(trials) / block_size)
-        self.trial_list = []
+    def alternate_blocks(self, cond1_trials, cond2_trials):
+        """
+        Alternate conditions blockwise
+        :param cond1_trials:
+        :param cond2_trials:
+        """
+        n_blocks = int((len(cond1_trials) + len(cond2_trials)
+                        / self.block_size))
         for block in range(n_blocks):
-            i_start = block * block_size
-            i_end = (block + 1) * block_size
-            sublist = first_half[i_start:i_end]
+            i_start = block * self.block_size
+            i_end = (block + 1) * self.block_size
+            sublist = cond1_trials[i_start:i_end]
             for item in sublist:
                 self.trial_list.append(item)
-            sublist = second_half[i_start:i_end]
+            sublist = cond2_trials[i_start:i_end]
             for item in sublist:
                 self.trial_list.append(item)
 
@@ -50,12 +63,26 @@ class BlockStim:
         """
         return self.trial_list[trial_nr]
 
+    def trial_missed(self, trial):
+        """
+        Add missed trials to list for repeating later
+        :param trial:
+        """
+        if trial[2] == 0:
+            self.no_gvs_missed.append(trial)
+        else:
+            self.gvs_missed.append(trial)
+
+    def rerun_missed_trials(self):
+        self.alternate_blocks(self.gvs_missed, self.no_gvs_missed)
+        # NOTE: account for variable lengths lists
+
 
 if __name__ == "__main__":
-    stimulus_range = [5, 4, 3, 2]
+    stim_range = [5, 4, 3, 2]
     repeats = [2, 2, 2, 2]
     cond = {"frame_angles": ["noframe", 20.0], "currents": [0.0, 1.0],
             "block_size": 4}
-    s = BlockStim(stimulus_range, repeats, **cond)
+    s = BlockStim(stim_range, repeats, **cond)
     print(s.trial_list)
     print(len(s.trial_list))
